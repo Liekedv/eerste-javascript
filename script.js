6c7cb440fb180bd4cf944dafb27dbc9e812aa292
Liekedv <84664965+Liekedv@users.noreply.github.com>
2021-06-27 11:04:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Hello Winc Academy")
+let name = 'Lieke';
+console.log(name);
+let number = 4;
+let getal = 2;
+console.log(number + number);
+console.log(number * number - number);
+console.log(number / getal);
+console.log(number % getal);
+// typeof
+let leeftijd = 28;
+let leeftijdUitgeschreven ='achtentwintig'
+console.log(typeof leeftijd);
+console.log(typeof leeftijdUitgeschreven);
